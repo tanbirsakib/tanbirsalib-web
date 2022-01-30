70da4c91f656ef5ba6591e887fa8a845b2d751af
Tanbir Sakib <tanbirsakib>
2022-01-30 15:01:17 +0600

    navbar icons added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import Navbar from '../Shared/Navbar/Navbar';
+import github from '../../assets/kissclipart-brand-icon-github-icon-logo-icon-43744a01ea5d154a.png'
 
 const Home = () => {
     return (
-        <div className='bg-sky-600 p-1 '>
+        <div className='bg-sky-600 pb-2'>
             <Navbar></Navbar>
-            <div className='border border-white rounded  w-2/6 mx-auto'>
-                <h2 className='text-white ml-2'>info.html</h2>
-                <hr />
-                <pre className='text-white p-1'>
-                    <h1 className='text-xl'>
-                        &lt;h1&gt; Hi, I'm Tanbir Sakib 👨🏼‍💻 &lt;/h1&gt;
-                    </h1>
-                    &lt;ul&gt;
-                    <h2 className='font-mono ml-4'>
-                        &lt;li&gt; Web Developer ⚡ &lt;/li&gt;
-                    </h2>
-                    <h2 className='font-mono ml-4'>
-                        &lt;li&gt; I love to code 🤞 &lt;/li&gt;
-                    </h2>
+            <br />
+            <br />
+            <section className=''>
+                
+                 <div className=''>
+                 <a className='flex flex-col' href="https://github.com/tanbirsakib" rel='noreferrer' target="_blank">
+                    <div>
+                        <img className='w-1/12' src={github} alt="" />
+                        <p className='text-xl text-white font-semibold font-mono inline'>Github</p>
+                    </div>
+                    </a>
+                 </div>
+                    
+               
+                <div className='  border border-white rounded w-2/6'>
+                    <h2 className='text-white ml-2'>info.html</h2>
+                    <hr />
+                    <pre className='text-white p-1 bg-sky-700'>
+                        <h1 className='text-xl'>
+                            &lt;h1&gt; Hi, I'm <span className='text-2xl'>Tanbir Sakib</span> 👨🏼‍💻 &lt;/h1&gt;
+                        </h1>
+                        &lt;ul&gt;
+                        <h2 className='font-mono ml-4'>
+                            &lt;li&gt; Web Developer ⚡ &lt;/li&gt;
+                        </h2>
+                        <h2 className='font-mono ml-4'>
+                            &lt;li&gt; I love to code 🤞 &lt;/li&gt;
+                        </h2>
 
-                    &lt;/ul&gt;
+                        &lt;/ul&gt;
 
-                </pre>
+                    </pre>
 
-            </div>
+                </div>
+            </section>
         </div>
     );
 };
--- src/components/Shared/Navbar/Navbar.js
+++ src/components/Shared/Navbar/Navbar.js
@@ -4,15 +4,15 @@ import './navbar.css'
 
 const Navbar = () => {
     return (
-     <div className='flex justify-between'>
+     <div className='flex justify-between bg-sky-700'>
          <div className='p-1'>
              <NavLink to='/home' className='text-white text-4xl font-semibold'>TS</NavLink>
          </div>
             <div className='flex justify-end'>
-           <NavLink className="mr-2 hover:text-blue-300 text-xl font-mono text-white" to='/home'>Home</NavLink>
-           <NavLink className="mr-2 hover:text-blue-300 text-xl font-mono text-white" to='/about'>About</NavLink>
-           <NavLink className="mr-2 hover:text-blue-300 text-xl font-mono text-white" to='/projects'>Projects</NavLink>
-           <NavLink className="mr-2 hover:text-blue-300 text-xl font-mono text-white" to='/contact'>Contact</NavLink>
+           <NavLink className="mr-3 hover:text-blue-300 text-xl font-mono text-white" to='/home'>Home🏠</NavLink>
+           <NavLink className="mr-3 hover:text-blue-300 text-xl font-mono text-white" to='/about'>About🌞</NavLink>
+           <NavLink className="mr-3 hover:text-blue-300 text-xl font-mono text-white" to='/projects'>Projects♒</NavLink>
+           <NavLink className="mr-3 hover:text-blue-300 text-xl font-mono text-white" to='/contact'>Contact💌</NavLink>
         </div>
      </div>
     );
